e149947bd8aee4ab745a81baf5099784e771152e
Anmol Gupta <anmol.gupta2180@gmail.com>
2023-01-13 11:03:55 -0500

    I have created model for user and thought

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect , connection} = require('mongoose');
+
+connect('mongodb://localhost/socialNetwork',{
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+
+module.exports= connection;
+
